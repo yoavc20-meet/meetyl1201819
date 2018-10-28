0c3c7e319d318f4cecba428ba024194b48a39eab
yoavc20-meet <yoavc20@meet.mit.edu>
2018-10-28 19:18:59 +0200

    lab4

--- yoavc20_yearlong/TurtleShapes.py
+++ yoavc20_yearlong/TurtleShapes.py
@@ -12,8 +12,10 @@ import turtle
 #CopyPaste()
 #CopyPaste()
 #CopyPaste()
+'''
 angle = 145
 length = 100
+
 def draw_star(angle, length):
 	for i in range(5):
 		turtle.left(angle)
@@ -21,10 +23,25 @@ def draw_star(angle, length):
 
 draw_star(angle, length)
 
+'''
 
+#exercise 2
+'''
+def upside_house():
+	turtle.begin_fill()
+	for x in range(4):
+		turtle.forward(40)
+		turtle.left(90)
+	for a in range(1):
+		turtle.left(-45)
+		turtle.forward(30)
+		turtle.left(90)
+		turtle.forward(30)
 
-
-
+	turtle.end_fill()
+upside_house()
+'''
+#exercise 3
 
 
 
---
+++ yoavc20_yearlong/lab3.py
@@ -0,0 +1,21 @@
+import turtle
+#exercise 3
+'''
+turtle.register_shape("abdalluh.gif")
+turtle.addshape("abdalluh.gif")
+turtle.shape("abdalluh.gif")
+print(turtle.getshapes())
+'''
+#exercise 4
+#turtle.left(90)
+turtle.forward(150)
+
+
+
+
+
+
+
+
+
+turtle.mainloop()
