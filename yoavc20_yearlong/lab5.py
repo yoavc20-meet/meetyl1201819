c3e32a10bc610d89f41f9c6cf95bb267faca02b1
yoavc20-meet <yoavc20@meet.mit.edu>
2018-11-11 19:25:52 +0200

    lab5

---
+++ yoavc20_yearlong/lab5.py
@@ -0,0 +1,12 @@
+#problem1
+'''
+import tkinter as tk
+from tkinter import simpledialog
+#Then when ever you want to ask the user for input use this code
+greeting = simpledialog.askstring("Input", "Hello, possible pirate! What's the password?", parent=tk.Tk().withdraw())
+if greeting in ("Arrr!"):
+    print("Go away, pirate.")
+elif():
+	print("Greetings, hater of pirates!")
+'''
+#problem2b
