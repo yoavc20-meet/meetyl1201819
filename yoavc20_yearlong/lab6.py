88b7d65ef1871d1a6a0fa883882e09269db33764
yoavc20-meet <yoavc20@meet.mit.edu>
2018-11-25 19:03:08 +0200

    lab 6

--- yoavc20_yearlong/lab6.py
+++ yoavc20_yearlong/lab6.py
@@ -1,10 +1,45 @@
 import turtle
 from turtle import Turtle
+import random
 
+turtle.colormode(255)
+'''
+R = 0
+B = 0
+G = 0
+'''
 class Square(Turtle):
-	def __init__(self, size, random_color):
+	def __init__(self, size):
+		Turtle. __init__(self)
+		self.shapesize(size)
+		self.shape("square")
+	def change_color(self):
+#		self.random_color = random_color
+		R = random.randint(0, 255)
+		B = random.randint(0, 255)
+		G = random.randint(0, 255)
+		self.color(R, B, G)	
+		#turtle.color(R, B, G)
+
+	'''
+	def pick_color():
+		self.random_color = random_color	
+		return choice(["red", "yellow", "blue"])
+'''
+#print(random.random())
+square1 = Square(25)
+square1.change_color()
+
+
+
+
+'''
+class Hexagon(Turtle):
+	def __init__(self,size):
 		Turtle. __init__(self)
 		self.size(size)
-		self.shape("Square")
-		self.random_color(random_color)
-		
+'''
+
+
+
+turtle.mainloop()
